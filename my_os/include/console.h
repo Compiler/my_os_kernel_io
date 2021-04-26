bf79cab6a53b500185c8e2e1000a3d2cb93dc615
compiler <lukerochedev@gmail.com>
2021-04-26 14:42:53 -0400

    test

--- my_os/include/console.h
+++ my_os/include/console.h
@@ -5,29 +5,31 @@
 
 
 
-#define CONSOLE_BLACK		    0x00
-#define CONSOLE_BLUE		    0x01
-#define CONSOLE_GREEN		    0x02
-#define CONSOLE_CYAN		    0x03
-#define CONSOLE_RED		        0x04
-#define CONSOLE_PURPLE		    0X05
-#define CONSOLE_BROWN		    0X06
-#define CONSOLE_GRAY		    0X07
-#define CONSOLE_DARK_GRAY	    0X08
-#define CONSOLE_LIGHT_BLUE	    0X09
-#define CONSOLE_LIGHT_GREEN	    0X0A
-#define CONSOLE_LIGHT_CYAN	    0X0B
-#define CONSOLE_LIGHT_RED	    0X0C
+#define CONSOLE_BLACK           0x00
+#define CONSOLE_BLUE            0x01
+#define CONSOLE_GREEN           0x02
+#define CONSOLE_CYAN            0x03
+#define CONSOLE_RED             0x04
+#define CONSOLE_PURPLE          0X05
+#define CONSOLE_BROWN           0X06
+#define CONSOLE_GRAY            0X07
+#define CONSOLE_DARK_GRAY       0X08
+#define CONSOLE_LIGHT_BLUE      0X09
+#define CONSOLE_LIGHT_GREEN     0X0A
+#define CONSOLE_LIGHT_CYAN      0X0B
+#define CONSOLE_LIGHT_RED       0X0C
 #define CONSOLE_LIGHT_PURPLE    0X0D
-#define CONSOLE_YELLOW		    0X0E
-#define CONSOLE_WHITE		    0X0F
+#define CONSOLE_YELLOW          0X0E
+#define CONSOLE_WHITE           0X0F
 
-#define CONSOLE_COLOR_DEFAULT	CONSOLE_LIGHT_GREEN
+
+#define BYTES_PER_CHAR           2
+#define CONSOLE_COLOR_DEFAULT   CONSOLE_LIGHT_GREEN
 static int _console_color = CONSOLE_COLOR_DEFAULT;
 
 static char* const VGA_MEMORY = (char*) 0xb8000;
-static const int VGA_WIDTH =	80;
-static const int VGA_HEIGHT=	25;
+static const int VGA_WIDTH =    80;
+static const int VGA_HEIGHT=    25;
 
 extern unsigned int _position;
 
@@ -37,6 +39,6 @@ void new_line();
 void print_character(char);
 void print_string(char*);
 void print_line(char*);
+void shift();
 
-
-#endif 
+#endif
--- my_os/include/shell/console.c
+++ my_os/include/shell/console.c
@@ -1,37 +1,42 @@
-#include "../console.h"
+#include "console.h"
 
 
 unsigned int _position = 0;
+
+
+void shift(){
+    for(int pos = 0; pos < (VGA_WIDTH * VGA_HEIGHT * BYTES_PER_CHAR) - (VGA_WIDTH * BYTES_PER_CHAR); pos++){
+        VGA_MEMORY[pos] = VGA_MEMORY[pos + VGA_WIDTH * BYTES_PER_CHAR];
+    }
+
+
+}
+
 void new_line(){
-	_position = 2 * ((_position/2) + (VGA_WIDTH - ((_position/2) % VGA_WIDTH)));
+    _position = (2 * ((_position/2) + (VGA_WIDTH - ((_position/2) % VGA_WIDTH))));
 }
 void print_character(char character){
-	if(character == '\n'){
-		new_line();
-		return;
-	}
-	VGA_MEMORY[_position++] = character;
-	VGA_MEMORY[_position++] = _console_color;
+    if(character == '\n'){
+            new_line();
+            return;
+    }
+    VGA_MEMORY[_position++] = character;
+    VGA_MEMORY[_position++] = _console_color;
 }
 
 void print_string(char* str){
-	unsigned int i = 0;
-	while(str[i++] != NULL_TERMINATE){
-		if(str[i - 1] == '\n'){
-			new_line();
-			continue;
-		}
-		VGA_MEMORY[_position++] = str[i - 1];
-		VGA_MEMORY[_position++] = _console_color;
-	} 
+    unsigned int i = 0;
+    while(str[i++] != NULL_TERMINATE){
+            print_character(str[i-1]);
+    }
 }
 
-void print_line(char* str){	
-	print_string(str);
-	new_line();
+void print_line(char* str){
+    print_string(str);
+    new_line();
 }
 
 
 void set_console_color(int newColor){
-	_console_color = newColor;
+    _console_color = newColor;
 }
